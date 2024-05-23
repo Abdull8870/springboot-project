d6de561bba99910b513f9844b0141ef0032ba286
Abdul Rahuman <abrahuman8870@gmail.com>
2024-05-22 23:42:18 -0400

    Added validation for school name argument

--- src/main/java/com/restapi/application/school/SchoolDTO.java
+++ src/main/java/com/restapi/application/school/SchoolDTO.java
@@ -1,6 +1,9 @@
 package com.restapi.application.school;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record SchoolDTO(
+        @NotEmpty(message = "School Name Cannot be Empty")
         String schoolName
 ) {
 }
